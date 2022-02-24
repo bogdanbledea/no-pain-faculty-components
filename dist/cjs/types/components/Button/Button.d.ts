65ea4d0a7c6d2410abb1c05c06d7f7d1e2524600
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2022-02-24 08:42:39 +0200

    Remove dist folder from git and move dep to devDependencies

--- dist/cjs/types/components/Button/Button.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-declare type ButtonProps = {
-    children: any;
-    onClick?: () => void;
-};
-declare const Button: (props: ButtonProps) => JSX.Element;
-export default Button;
--- dist/cjs/types/components/Button/Button.stories.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="react" />
-import { ComponentStory, ComponentMeta } from '@storybook/react';
-import Button from './Button';
-declare const _default: ComponentMeta<(props: {
-    children: any;
-    onClick?: (() => void) | undefined;
-}) => JSX.Element>;
-export default _default;
-export declare const Primary: ComponentStory<typeof Button>;
