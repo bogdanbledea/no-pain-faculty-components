8ac8fd185da78ee9270b19b29b9b6b1455cc9824
Bogdan Bledea <bledeabogdanalexandru@gmail.com>
2022-02-27 22:37:24 +0200

    Add preserveModules to rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -10,6 +10,7 @@ const packageJson = require("./package.json");
 export default [
   {
     input: "src/index.ts",
+    preserveModules: true,
     output: [
       {
         file: packageJson.main,
